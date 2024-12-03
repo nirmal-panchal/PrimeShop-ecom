a6a71ca2ba07632c4d9f0dcc471d8ccb2e5e1ad9
nirmal-panchal <nirmalpanchal85@gmail.com>
2024-12-03 23:28:01 +0530

    fixed build errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,7 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
-
+import { cn } from "../../lib/utils";
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
     return (
@@ -14,9 +13,9 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
